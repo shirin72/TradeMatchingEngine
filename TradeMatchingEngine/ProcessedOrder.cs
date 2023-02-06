7278f90bd725ba90e4cbf43a8fc593e9350c96a5
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-02-06 16:02:30 +0330

    add ProcessOrder

---
+++ TradeMatchingEngine/ProcessedOrder.cs
@@ -0,0 +1,11 @@
+﻿using TradeMatchingEngine;
+
+namespace TradeMatchingEngine
+{
+    public class ProcessedOrder
+    {
+        public long OrderId { get; set; }
+
+        public IEnumerable<Trade>? Trades { get; set; }
+    }
+}
