50bb8e22f26978c2da51f1c98f570449d697b9ba
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-02-25 09:20:57 +0330

    fix bug disposed object

--- SpecFlowTest/HttpClientWorker.cs
+++ SpecFlowTest/HttpClientWorker.cs
@@ -14,7 +14,7 @@ namespace SpecFlowTest
         {
             var services = new ServiceCollection();
             services.AddHttpClient();
-            
+
             var containerBuilder = new ContainerBuilder();
             containerBuilder.Populate(services);
             var container = containerBuilder.Build();
@@ -27,22 +27,21 @@ namespace SpecFlowTest
             var serializer = new JsonSerializer() { ContractResolver = new CamelCasePropertyNamesContractResolver() };
             var request = new HttpRequestMessage(httpMethod, url);
 
-            if (httpMethod != HttpMethod.Patch && input as object != null)
-            {
-                using var ms = new MemoryStream();
-                using var writer = new StreamWriter(ms);
-                using var textWriter = new JsonTextWriter(writer);
+            using var ms = new MemoryStream();
+            using var writer = new StreamWriter(ms);
+            using var textWriter = new JsonTextWriter(writer);
 
-                serializer.Serialize(textWriter, input);
-                textWriter.Flush();
-                ms.Seek(0, SeekOrigin.Begin);
-                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                using var requestContent = new StreamContent(ms);
-                request.Content = requestContent;
-                requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            }
+            serializer.Serialize(textWriter, input);
+            textWriter.Flush();
+            ms.Seek(0, SeekOrigin.Begin);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using var requestContent = new StreamContent(ms);
+
+            request.Content = requestContent;
+            requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
             using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStreamAsync();
             using var reader = new StreamReader(content);
@@ -50,34 +49,5 @@ namespace SpecFlowTest
 
             return serializer.Deserialize<TOutput>(textReader);
         }
-
-
-        //public async static Task<object> Execute(string url, HttpMethod httpMethod)
-        //{
-        //    var httpClient = factory.CreateClient();
-        //    //using var ms = new MemoryStream();
-        //    //using var writer = new StreamWriter(ms);
-        //    //using var textWriter = new JsonTextWriter(writer);
-
-        //    var serializer = new JsonSerializer() { ContractResolver = new CamelCasePropertyNamesContractResolver() };
-        //    //serializer.Serialize(textWriter, string.Empty);
-        //    //textWriter.Flush();
-        //    //ms.Seek(0, SeekOrigin.Begin);
-
-        //    var request = new HttpRequestMessage(httpMethod, url);
-        //    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //    //using var requestContent = new StreamContent(ms);
-        //    //request.Content = requestContent;
-
-        //    //requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-        //    using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-        //    response.EnsureSuccessStatusCode();
-        //    var content = await response.Content.ReadAsStreamAsync();
-        //    using var reader = new StreamReader(content);
-        //    using var textReader = new JsonTextReader(reader);
-        //    return serializer.Deserialize(textReader);
-        //}
-
-
     }
 }
