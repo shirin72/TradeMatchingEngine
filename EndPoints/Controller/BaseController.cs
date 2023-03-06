974a6a712cefdc24ff569084cb8a89aad4da9fad
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-03-06 15:47:58 +0330

    clean code

--- EndPoints/Controller/BaseController.cs
+++ EndPoints/Controller/BaseController.cs
@@ -1,23 +1,104 @@
-﻿using EndPoints.Model;
+﻿using Domain;
+using EndPoints.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EndPoints.Controller
 {
-  
+
     public class BaseController : ControllerBase
     {
-        protected List<LinkVM> Links(List<Tuple<string, string, string, object?>> linkDtos)
+        protected ProcessedOrderVM CreateResponse(ProcessedOrder? result, List<Tuple<string, string, string, object?>> linkDtos)
         {
-            var linkDto = new List<LinkVM>();
-
-            foreach (var item in linkDtos)
+            var lst = new List<TradeVM>();
+            if (result.Trades != null)
             {
-                linkDto.Add(new LinkVM(Url.RouteUrl(item.Item1, item.Item4), item.Item2, item.Item3));
+                foreach (var item in result.Trades)
+                {
+                    lst.Add(new TradeVM()
+                    {
+                        Amount = item.Amount,
+                        BuyOrderId = item.BuyOrderId,
+                        Id = item.Id,
+                        Price = item.Price,
+                        SellOrderId = item.SellOrderId,
+                    });
+                }
+                List<TradeVM> trades = CreateTrades(lst, linkDtos.Where(l => l.Item1 == nameof(TradesController.GetTrade)).ToList());
             }
 
-            return linkDto;
+            return CreateProcessedOrder(result, lst, linkDtos.Where(l => l.Item1 != nameof(TradesController.GetTrade)).ToList());
         }
+        protected ProcessedOrderVM CreateProcessedOrder(ProcessedOrder? result, List<TradeVM> trades, List<Tuple<string, string, string, object?>> linkDtos)
+        {
+            return new ProcessedOrderVM()
+            {
+                CancelledOrders = result.CancelledOrders,
+                RegisteredOrder = CreateRegisteredOrder(result.OrderId, linkDtos),
+                Trades = trades,
+            };
+        }
+        protected RegisteredOrderVM CreateRegisteredOrder(long orderId, List<Tuple<string, string, string, object?>> linkDtos)
+        {
+            return new RegisteredOrderVM()
+            {
+                OrderId = orderId,
+                Links = CreateLinks(linkDtos)
+            };
+        }
+        protected List<LinkVM> CreateLinks(List<Tuple<string, string, string, object?>> linkDtos)
+        {
+            var links = new List<LinkVM>();
 
+            foreach (var link in linkDtos)
+            {
+                links.Add(new LinkVM(Url.Link(link.Item1, new { id = link.Item4 }), link.Item2, link.Item3));
+            }
+
+            return links;
+        }
+        protected List<TradeVM> CreateTrades(List<TradeVM>? trades, List<Tuple<string, string, string, object?>> linkDtos)
+        {
+            if (trades != null)
+            {
+                trades.ForEach(t => t.Link = CreateLinks(linkDtos));
+            }
+
+            return trades;
+        }
+        protected List<Tuple<string, string, string, object?>> addAllowedProcessOrderLinks(List<Tuple<string, string, string, object?>> tuples, IEnumerable<ITrade> trades = null, long? orderId = null)
+        {
+            tuples.Add(new(nameof(OrdersController.CancellOrder), "self", "DELETE", orderId));
+            tuples.Add(new(nameof(OrdersController.GetOrder), "self", "Get", orderId));
+            tuples.Add(new(nameof(OrdersController.ModifyOrder), "self", "PUT", null));
+
+            foreach (var trade in trades)
+            {
+                tuples.Add(new(nameof(TradesController.GetTrade), "self", "Get", trade.Id));
+            }
+
+            return tuples;
+        }
+        protected List<Tuple<string, string, string, object?>> addAllowedModifyOrderLinks(List<Tuple<string, string, string, object?>> tuples, IEnumerable<ITrade> trades = null, long? orderId = null)
+        {
+            tuples.Add(new(nameof(OrdersController.CancellOrder), "self", "DELETE", orderId));
+            tuples.Add(new(nameof(OrdersController.GetOrder), "self", "Get", orderId));
+            tuples.Add(new(nameof(OrdersController.ProcessOrder), "self", "Post", null));
+            if (trades != null)
+            {
+                foreach (var trade in trades)
+                {
+                    tuples.Add(new(nameof(TradesController.GetTrade), "self", "Get", trade.Id));
+                }
+            }
+
+            return tuples;
+        }
+        protected List<Tuple<string, string, string, object?>> addAllowedGetOrderLinks(List<Tuple<string, string, string, object?>> tuples, IEnumerable<ITrade> trades = null, long? orderId = null)
+        {
+            tuples.Add(new(nameof(OrdersController.CancellOrder), "self", "DELETE", orderId));
+            tuples.Add(new(nameof(OrdersController.ProcessOrder), "self", "Post", null));
+            return tuples;
+        }
         protected List<Tuple<string, string, string, object?>> createTupleObject()
         {
             return new List<Tuple<string, string, string, object?>>();
