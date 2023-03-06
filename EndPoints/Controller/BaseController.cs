9f6cbca8fb9e301ec7b00223e3ac3c4bd8f3fd22
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-03-06 16:19:20 +0330

    clean code

--- EndPoints/Controller/BaseController.cs
+++ EndPoints/Controller/BaseController.cs
@@ -7,6 +7,16 @@ namespace EndPoints.Controller
 
     public class BaseController : ControllerBase
     {
+        private static readonly string HTTPMETHOD_GET = "GET";
+        private static readonly string HTTPMETHOD_POST = "POST";
+        private static readonly string HTTPMETHOD_PUT = "PUT";
+        private static readonly string HTTPMETHOD_PATCH = "PATCH";
+        private static readonly string HTTPMETHOD_DELETE = "DELETE";
+        private static readonly string REL_CANCELL_ORDER = "cancell_order";
+        private static readonly string REL_GET_ORDER = "get_order";
+        private static readonly string REL_GET_TRADE = "get_trade";
+        private static readonly string REL_PUT_ORDER = "modify_order";
+        private static readonly string REL_POST_ORDER = "post_order";
         protected ProcessedOrderVM CreateResponse(ProcessedOrder? result, List<Tuple<string, string, string, object?>> linkDtos)
         {
             var lst = new List<TradeVM>();
@@ -23,6 +33,7 @@ namespace EndPoints.Controller
                         SellOrderId = item.SellOrderId,
                     });
                 }
+
                 List<TradeVM> trades = CreateTrades(lst, linkDtos.Where(l => l.Item1 == nameof(TradesController.GetTrade)).ToList());
             }
 
@@ -67,27 +78,27 @@ namespace EndPoints.Controller
         }
         protected List<Tuple<string, string, string, object?>> addAllowedProcessOrderLinks(List<Tuple<string, string, string, object?>> tuples, IEnumerable<ITrade> trades = null, long? orderId = null)
         {
-            tuples.Add(new(nameof(OrdersController.CancellOrder), "self", "DELETE", orderId));
-            tuples.Add(new(nameof(OrdersController.GetOrder), "self", "Get", orderId));
-            tuples.Add(new(nameof(OrdersController.ModifyOrder), "self", "PUT", null));
+            tuples.Add(new(nameof(OrdersController.CancellOrder), REL_CANCELL_ORDER, HTTPMETHOD_DELETE, orderId));
+            tuples.Add(new(nameof(OrdersController.GetOrder), REL_GET_ORDER, HTTPMETHOD_GET, orderId));
+            tuples.Add(new(nameof(OrdersController.ModifyOrder), REL_PUT_ORDER, HTTPMETHOD_PUT, null));
 
             foreach (var trade in trades)
             {
-                tuples.Add(new(nameof(TradesController.GetTrade), "self", "Get", trade.Id));
+                tuples.Add(new(nameof(TradesController.GetTrade), REL_GET_TRADE, HTTPMETHOD_GET, trade.Id));
             }
 
             return tuples;
         }
         protected List<Tuple<string, string, string, object?>> addAllowedModifyOrderLinks(List<Tuple<string, string, string, object?>> tuples, IEnumerable<ITrade> trades = null, long? orderId = null)
         {
-            tuples.Add(new(nameof(OrdersController.CancellOrder), "self", "DELETE", orderId));
-            tuples.Add(new(nameof(OrdersController.GetOrder), "self", "Get", orderId));
-            tuples.Add(new(nameof(OrdersController.ProcessOrder), "self", "Post", null));
+            tuples.Add(new(nameof(OrdersController.CancellOrder), REL_CANCELL_ORDER, HTTPMETHOD_DELETE, orderId));
+            tuples.Add(new(nameof(OrdersController.GetOrder), REL_GET_ORDER, HTTPMETHOD_GET, orderId));
+            tuples.Add(new(nameof(OrdersController.ProcessOrder), REL_POST_ORDER, HTTPMETHOD_POST, null));
             if (trades != null)
             {
                 foreach (var trade in trades)
                 {
-                    tuples.Add(new(nameof(TradesController.GetTrade), "self", "Get", trade.Id));
+                    tuples.Add(new(nameof(TradesController.GetTrade), REL_GET_TRADE, HTTPMETHOD_GET, trade.Id));
                 }
             }
 
@@ -95,8 +106,8 @@ namespace EndPoints.Controller
         }
         protected List<Tuple<string, string, string, object?>> addAllowedGetOrderLinks(List<Tuple<string, string, string, object?>> tuples, IEnumerable<ITrade> trades = null, long? orderId = null)
         {
-            tuples.Add(new(nameof(OrdersController.CancellOrder), "self", "DELETE", orderId));
-            tuples.Add(new(nameof(OrdersController.ProcessOrder), "self", "Post", null));
+            tuples.Add(new(nameof(OrdersController.CancellOrder), REL_CANCELL_ORDER, HTTPMETHOD_DELETE, orderId));
+            tuples.Add(new(nameof(OrdersController.ProcessOrder), REL_POST_ORDER, HTTPMETHOD_POST, null));
             return tuples;
         }
         protected List<Tuple<string, string, string, object?>> createTupleObject()
